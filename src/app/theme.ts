c6eabb2d771d2f611e9fe7db5be24b23f8045ae5
Sheetal575 <sheetaldadh9@gmail.com>
2024-03-11 23:53:09 +0530

    themes integrated

---
+++ src/app/theme.ts
@@ -0,0 +1,16 @@
+const theme:any = {
+  light: {
+    primaryColor: "#007bff",
+    secondaryColor: "#6c757d",
+    backgroundColor: "#ffffff",
+    textColor: "#000000",
+  },
+  dark: {
+    primaryColor: "#17a2b8",
+    secondaryColor: "#adb5bd",
+    backgroundColor: "#343a40",
+    textColor: "#ffffff",
+  },
+};
+
+export default theme;
